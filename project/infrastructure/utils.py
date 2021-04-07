ac16c9784ff2a6117054a9a93b37624a05aaca3a
yifanwu2828 <yiw084@ucsd.edu>
2021-04-07 16:39:02 -0400

    add im_show

--- project/infrastructure/utils.py
+++ project/infrastructure/utils.py
@@ -12,3 +12,10 @@ def calc_n_out(n_in: int, k: int, s: int, p: int):
     :return n_out: num of output features
     """
     return math.floor((n_in + 2*p - k) / s) + 1
+
+
+def im_show(img: torch.Tensor):
+    # img = img / 2 + 0.5  # unnormalize
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1, 2, 0)))
+    plt.show()
