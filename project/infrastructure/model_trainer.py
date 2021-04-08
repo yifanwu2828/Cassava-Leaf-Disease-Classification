2ca43304d19029f7b479629eb68eb301388d0e3d
yifanwu2828 <yiw084@ucsd.edu>
2021-04-08 19:09:31 -0400

    add validation

--- project/infrastructure/model_trainer.py
+++ project/infrastructure/model_trainer.py
@@ -1,4 +1,5 @@
 import abc
+from collections import defaultdict
 import time
 
 import numpy as np
@@ -40,13 +41,13 @@ class AverageMeter:
         self.avg = self.sum / self.count
 
 
-
 class Model(nn.Module, metaclass=abc.ABCMeta):
     """ simple model trainer """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Param Dict
         self.params = None
 
         # Dataset
@@ -65,6 +66,7 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
         self.optimizer = None
         self.scheduler = None
 
+        # history record
         self.current_epoch = 0
         self.current_train_step = 0
         self.current_valid_step = 0
@@ -72,6 +74,7 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
         # GPU or CPU
         self.device = None
         self.num_workers = None
+
         # Use automatic mixed precision training in GPU
         self.fp16 = False
         self.scaler = None
@@ -80,10 +83,12 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
         self.start_time = None
         self.end_time = None
 
+        self.metrics = {"train": {}, "valid": {}, "test": {}}
+
     #######################################################
 
     def init_trainer(self, params: dict):
-        """ Init"""
+        """ Init Trainer"""
         self.params = params
 
         # Set random seeds
@@ -128,15 +133,16 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
 
     def model_fn(self, batch):
         data, targets = batch
-        data = data.to(device=self.device)
-        targets = targets.to(device=self.device)
+        data = data.to(device=self.device, non_blocking=True)
+        targets = targets.to(device=self.device, non_blocking=True)
 
-        metrics = None
         if self.fp16:
             with torch.cuda.amp.autocast():
                 output, loss = self(data, targets)
         else:
             output, loss = self(data, targets)
+
+        metrics = self.monitor_metrics(output, targets)
         return output, loss, metrics
 
     #####################################################################
@@ -156,9 +162,11 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
     ):
         """ fit the model """
         self.fp16 = use_fp16
-        if num_workers == -1:
-            num_workers = psutil.cpu_count()
-        self.num_workers = num_workers
+
+        # use all workers if -1
+        self.num_workers = psutil.cpu_count() if num_workers == -1 else num_workers
+        pin_memory = True if torch.cuda.is_available() else False
+
         if self.train_loader is None:
             self.train_loader = DataLoader(
                 dataset=train_dataset,
@@ -166,9 +174,9 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
                 shuffle=shuffle,
                 sampler=train_sampler,
                 batch_sampler=None,
-                num_workers=num_workers,
+                num_workers=self.num_workers,
                 collate_fn=None,
-                pin_memory=False
+                pin_memory=pin_memory
             )
 
         if self.valid_loader is None and valid_dataset is not None:
@@ -178,10 +186,11 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
                 shuffle=False,  # not going to shuffle data for validation
                 sampler=valid_sampler,
                 batch_sampler=None,
-                num_workers=num_workers,
+                num_workers=self.num_workers,
                 collate_fn=None,
-                pin_memory=False
+                pin_memory=pin_memory
             )
+
         self.device = device
         if next(self.parameters()).device != device:
             self.to(device)
@@ -192,44 +201,72 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
         if self.scheduler is None:
             self.scheduler = self.fetch_scheduler()
 
+        # device indicator for progress bar
+        if torch.cuda.is_available():
+            DEVICE = 'AMP' if self.fp16 else 'cuda'
+        else:
+            DEVICE = 'cpu'
+
+        history = defaultdict(list)
         self.start_time = time.time()
         n_epochs_loop = tqdm(range(max_epochs), desc="LDC", leave=True)
-        for epoch in n_epochs_loop:
-            train_epoch_loss = self.train_one_epoch(self.train_loader)
+        for _ in n_epochs_loop:
+
+            # Training Phase
+            train_loss, _ = self.train_one_epoch(self.train_loader)
+            train_loss = np.mean(train_loss)
+            history["train_loss"].append(train_loss)
+
+            # Validation phase
+            if self.valid_loader:
+                val_loss, val_metrics = self.validate_one_epoch(self.valid_loader)
+                val_loss = np.mean(val_loss)
+                history["val_loss"].append(val_loss)
+
+                for k, v in val_metrics.items():
+                    val_metrics[k]= np.mean(v)
+                    history[k].append(v)
+            else:
+                val_loss = {}
+                val_metrics = {}
 
             # update progress bar
-            DEVICE = 'AMP' if self.fp16 else 'cuda'
-            description = f'({DEVICE}) epoch {epoch} loss: {train_epoch_loss:.4f}'
+            description = f'({DEVICE}) epoch: {self.current_epoch}'
             n_epochs_loop.set_description(description)
+            n_epochs_loop.set_postfix(
+                train_loss=train_loss,
+                val_loss=val_loss,
+                **val_metrics
+            )
+            self.current_epoch += 1
         self.end_time = time.time() - self.start_time
+        return history
 
     def train_one_epoch(self, train_loader):
         """ train_one_epoch """
         self.train()
-        losses = AverageMeter()
-        metrics_meter = None
+        train_losses = []
+        train_metrics = None
+        # train_metrics = defaultdict(list)
 
-        for batch_idx, train_batch in enumerate(train_loader):
+        # tk0 = tqdm(train_loader, total=len(train_loader), leave=False)
+        tk0 = train_loader
+        for batch_idx, train_batch in enumerate(tk0):
             # train_batch is a tuple: (data, targets)
             loss, metrics = self.train_one_step(train_batch)
-            losses.update(loss.item(), train_loader.batch_size)
-
-            # if batch_idx == 0:
-            #     metrics_meter = {k: AverageMeter() for k in metrics}
-            #
-            # monitor = {}
-            # for m_m in metrics_meter:
-            #     metrics_meter[m_m].update(metrics[m_m], data_loader.batch_size)
-            #     monitor[m_m] = metrics_meter[m_m].avg
+            train_losses.append(ptu.to_numpy(loss))
+            # for k, v in metrics.items():
+            #     train_metrics[k].append(v)
             # self.current_train_step += 1
-            # tk0.set_postfix(loss=losses.avg, stage="train", **monitor)
-            # tk0.close()
-        return losses.avg
+        return train_losses, train_metrics
 
     def train_one_step(self, batch):
         """ take one gradient step """
+        loss: torch.Tensor
+        metrics: dict
+
         self.optimizer.zero_grad()
-        _, loss, metrics= self.model_fn(batch)
+        _, loss, metrics = self.model_fn(batch)
 
         with torch.set_grad_enabled(True):
             # USE AUTOMATIC MIXED PRECISION
@@ -244,6 +281,7 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
 
             if self.scheduler:
                 self.scheduler.step()
+
         return loss, metrics
 
     def monitor_metrics(self, *args, **kwargs):
@@ -254,94 +292,23 @@ class Model(nn.Module, metaclass=abc.ABCMeta):
         _, loss, metrics = self.model_fn(data)
         return loss, metrics
 
-    def validate_one_epoch(self, data_loader):
+    def validate_one_epoch(self, valid_loader):
         self.eval()
-        losses = AverageMeter()
-        for batch_idx, batch in enumerate(data_loader):
+        val_losses = []
+        val_metrics = defaultdict(list)
+
+        # tk0 = valid_loader
+        # tk0 = tqdm(valid_loader, total=len(valid_loader))
+        tk0 =  valid_loader
+        for batch_idx, batch in enumerate(tk0):
             with torch.no_grad():
                 loss, metrics = self.validate_one_step(batch)
-            losses.update(loss.item(), data_loader.batch_size)
-
-        return losses.avg
+            val_losses.append(ptu.to_numpy(loss))
+            for k, v in metrics.items():
+                val_metrics[k].append(v)
+            self.current_valid_step += 1
+        return val_losses, val_metrics
 
     def predict_one_step(self, data):
         output, _, _ = self.model_fn(data)
         return output
-
-###########################################
-
-class MyModel(Model):
-    def __init__(self, params: dict):
-        super().__init__()
-        self.params = params
-
-        self.conv1 = nn.Conv2d(
-            in_channels=params["input_size"],
-            out_channels=8,
-            kernel_size=(3, 3),
-            stride=(1, 1),
-            padding=(1, 1)
-        )
-        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        self.conv2 = nn.Conv2d(
-            in_channels=8,
-            out_channels=16,
-            kernel_size=(3, 3),
-            stride=(1, 1),
-            padding=(1, 1)
-        )
-        self.fc1 = nn.Linear(16 * 7 * 7, params["output_size"])
-
-    def fetch_optimizer(self):
-        """set optimizer"""
-        opt = optim.Adam(self.parameters(), lr=self.params["learning_rate"])
-        return opt
-
-    @staticmethod
-    def calc_loss(outputs, targets, criterion=None):
-        """ calculate loss """
-        if targets is None or criterion is None:
-            print("Targets is None or Criterion is None")
-            return None
-        return criterion(outputs, targets)
-
-    def forward(self, x, targets=None):
-        """
-        forward function return logits and loss
-        """
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.reshape(x.shape[0], -1)
-        out = self.fc1(x)
-
-        criterion = nn.CrossEntropyLoss()
-        loss = self.calc_loss(out, targets, criterion)
-
-        return out, loss
-
-    def check_accuracy(self, loader):
-        """ simple acc check"""
-        if loader.dataset.train:
-            print("Checking accuracy on training data")
-        else:
-            print("Checking accuracy on test data")
-        num_correct = 0
-        mum_samples = 0
-        self.eval()
-
-        with torch.no_grad():
-            for x, y in loader:
-                x = x.to(device=self.device)
-                y = y.to(device=self.device)
-
-                scores, _ = self(x, y)
-                # 64x10
-                _, preds = scores.max(1)
-                num_correct += torch.sum((preds == y), -1)
-                mum_samples += preds.size(0)
-            accuracy = float(num_correct) / float(mum_samples)
-            print(f"Got {num_correct} / {mum_samples} with accuracy: {accuracy * 100: .2f}%")
-        self.train()
-        return accuracy
